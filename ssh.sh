92bac6ab5898faf5a266fe500f2ed2f9260fd00f
Hui <64741346+wanghui5801@users.noreply.github.com>
2024-10-23 14:31:16 +1100

    Update ssh.sh
    fix

--- ssh.sh
+++ ssh.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
 
 # Enter the key
-echo "Please enter the SSH public key (*.pub):"
+echo "Please enter the ssh public key(*.pub):"
 read -r ssh_key
 
-# Add key to /root/.ssh/authorized_keys
+# add key to  /root/.ssh/authorized_keys 中
 mkdir -p /root/.ssh
 echo "$ssh_key" >>/root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 chmod 700 /root/.ssh
 
 # Change /etc/ssh/sshd_config PermitRootLogin to prohibit-password
-if [ -f /etc/ssh/sshd_config ]; then
-  sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
-else
-  echo "sshd_config file not found. Exiting."
-  exit 1
-fi
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
 
 # Restart SSH
-if command -v systemctl &>/dev/null; then
-  systemctl restart sshd
-elif command -v service &>/dev/null; then
-  service ssh restart
-else
-  echo "Cannot determine how to restart SSH service. Please restart it manually."
-  exit 1
-fi
+systemctl restart sshd
 
 echo "Done"
